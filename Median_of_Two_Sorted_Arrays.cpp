9180d171874770d3205d20efe3b634ed89c52653
Yang Zongze <yangzongze@gmail.com>
2014-01-08 21:05:50 +0800

    Update Median_of_Two_Sorted_Arrays.cpp

--- Median_of_Two_Sorted_Arrays.cpp
+++ Median_of_Two_Sorted_Arrays.cpp
@@ -1,9 +1,13 @@
-/***************************************************************\
+/****************************************************************************\
 
-              Median of Two Sorted Arrays
+    Problem: Median of Two Sorted Arrays
+    Description: 
+        There are two sorted arrays A and B of size m and n respectively. 
+        Find the median of the two sorted arrays. The overall run time 
+        complexity should be O(log (m+n)).
+        
+    Name: 00797A7A
+    Email: yzz1215@live.cn
+    Create Date: 2013-01-09
 
-There are two sorted arrays A and B of size m and n respectively.
-Find the median of the two sorted arrays. The overall run time 
-complexity should be O(log (m+n)).
-
-\***************************************************************/
+\****************************************************************************/
