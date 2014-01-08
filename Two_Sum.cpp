7866cb1ec3aabf1db60e74fd861972f9267d0f4c
Yang Zongze <yangzongze@gmail.com>
2014-01-08 21:11:00 +0800

    Update Two_Sum.cpp

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -1,22 +1,25 @@
-/**************************************************************\
+/****************************************************************************\
 
-                            Two Sum
+    Problem: Two Sum
+    Description: 
+        Given an array of integers, find two numbers such that they add up to
+        a specific target number.
 
-Given an array of integers, find two numbers such that they add up to a specific 
-target number.
+        The function twoSum should return indices of the two numbers such that
+        they add up to the target, where index1 must be less than index2. 
+        Please note that your returned answers (both index1 and index2) are 
+        not zero-based.
 
-The function twoSum should return indices of the two numbers such that they add 
-up to the target, where index1 must be less than index2. Please note that your 
-returned answers (both index1 and index2) are not zero-based.
+        You may assume that each input would have exactly one solution.
 
-You may assume that each input would have exactly one solution.
+        Input: numbers={2, 7, 11, 15}, target=9
+        Output: index1=1, index2=2
+        
+    Name: 00797A7A
+    Email: yzz1215@live.cn
+    Create Date: 2013-01-09
 
-Input: numbers={2, 7, 11, 15}, target=9
-Output: index1=1, index2=2
-
-\*************************************************************/
-
-
+\****************************************************************************/
 
 class Solution {
 public:
