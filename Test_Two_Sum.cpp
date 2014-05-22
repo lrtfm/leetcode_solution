c6e739ab9ec2c8a95ce220d827feef4c2bf82189
Yang Zongze <yang@localhost.localdomain>
2014-05-23 01:02:40 +0800

    Cppunit Test

--- Test_Two_Sum.cpp
+++ Test_Two_Sum.cpp
@@ -2,42 +2,71 @@
 
 #include <iostream>
 #include <vector>
+#include <cstdarg>
+#include "cppunit/TestFixture.h"
+#include "cppunit/ui/text/TestRunner.h"
+#include "cppunit/TestCaller.h"
+#include "cppunit/TestSuite.h"
+
 using namespace std;
 
 #include "Two_Sum.cpp"
 
+vector<int> cvector(int n, ...)
+{
+    vector<int> ret;
+    va_list arg_ptr;
+    va_start(arg_ptr, n);
+    for (int i = 0; i < n; ++i) {
+        int value = va_arg(arg_ptr, int);
+        ret.push_back(value);
+    }
+    va_end(arg_ptr);
+
+    return ret;
+}    
+
+class MyTest : public CppUnit::TestFixture 
+{
+protected:
+    Solution solver;
+public:
+    MyTest() {}
+    void setUp() {}
+
+    void test01() 
+    {
+        vector<int> numbers = cvector(4, 15, 7, 11, 2);
+        vector<int> expect = cvector(2, 2, 4);
+        int target = 9;
+        vector<int> output = solver.twoSum(numbers, target);
+        CPPUNIT_ASSERT(expect == output);
+    }
+
+    void test02() 
+    {
+        vector<int> numbers = cvector(7, 1, 47, 9, 2, 12, 20, 23);
+        vector<int> expect = cvector(2, 3, 7);
+        int target = 70;
+        vector<int> output = solver.twoSum(numbers, target);
+        CPPUNIT_ASSERT(expect == output);
+    }
+
+    void tearDown() {}
+};
 
 /* test  code begin */
 int main(int argc, char *argv[])
 {
-    int i;
-    Solution solver;
-    vector<int> numbers(4, 0); 
-    numbers[0] = 15;
-    numbers[1] = 7;
-    numbers[2] = 11;
-    numbers[3] = 2;
+    CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestSuite *suite = new CppUnit::TestSuite();
 
-    int target = 9;
-    vector<int> expect_output(2, 0);
-    expect_output[0] = 2;
-    expect_output[1] = 4;
+    suite->addTest(new CppUnit::TestCaller<MyTest>(
+                "test01", &MyTest::test01));
+    suite->addTest(new CppUnit::TestCaller<MyTest>(
+                "test02", &MyTest::test02));
+    runner.addTest(suite);
+    runner.run("", true);
 
-    vector<int> output = solver.twoSum(numbers, target);
-    if ( expect_output.size() == output.size() ) {
-        for (i = 0; i < expect_output.size(); i++) {
-            if (expect_output[i] != output[i]) {
-                goto error;
-            }
-        }
-    }
-    else {
-        goto error;
-    }
-
-    cout<< "Test Correct" << endl;
-    return 0;
-error:
-    cout<< "Test Error" << endl;
     return 0;
 }
