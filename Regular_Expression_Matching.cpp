d1356e07aceca31983da955f3b857df3cc0d4712
Yang Zongze <yang@localhost.localdomain>
2014-05-25 11:18:22 +0800

    dp of Regular_Expression_Matching

--- Regular_Expression_Matching.cpp
+++ Regular_Expression_Matching.cpp
@@ -9,9 +9,55 @@
     Create Date: 2014年05月24日 19:05:39
 
 \****************************************************************************/
+#include <cstring>
+
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
+    bool isMatch /* dp */ (const char *s, const char *p) {
+        int len_s = strlen(s);
+        int len_p = strlen(p);
+
+        bool dp[len_p+1][len_s+1];
+        memset(dp, false, (len_p+1)*(len_s+1)*sizeof(bool));
+        dp[len_p][len_s] = true;
+        //for (int j = len_s - 1; j >= 0; j--) {
+        //    dp[len_p][j] = false;
+        //}
+        //for (int j = len_p - 1; j >= 0; j--) {
+        //    dp[j][len_s] = false;
+        //}
+        for (int i = len_p - 1; i >=0; i--) {
+            if (p[i] != '*') {
+                break;
+            }
+            i--;
+            //if (i < 0) { 
+            //    break;
+            //}
+            dp[i][len_s] = true;
+        }
+
+        for (int i = len_s - 1; i >= 0; i--) {
+            for (int k = len_p - 1; k >= 0; k--) {
+                if (p[k] != '*') {
+                    dp[k][i] = dp[k+1][i+1] && 
+                        (p[k] == '.' || p[k] == s[i]);
+                } else {
+                    k--;
+                    int j = 0; 
+                    dp[k][i] = dp[k+2][i];
+                    while (i + j < len_s && (p[k] == '.' || p[k] == s[i+j])) {
+                        if (dp[k][i]) break;
+                        dp[k][i] = dp[k][i] || dp[k+2][i+1+j] ;
+                        j++;
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    bool isMatch_r(const char *s, const char *p) {
         if (p == NULL || s == NULL) {
             return false;
         }
